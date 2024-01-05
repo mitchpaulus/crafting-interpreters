073a3e27eb1bf8929a3942466212f60a08e4b30d
Mitchell Paulus <paulusm14@gmail.com>
2024-01-05 08:03:31 -0600

    Init pg. 250

---
+++ build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+set -e
+gcc -o main *.c
+./main
---
+++ main.c
@@ -0,0 +1,21 @@
+#include "common.h"
+#include <stdio.h>
+#include "chunk.h"
+
+int main(int argc, const char* argv[]) {
+    printf("Hello World!\n");
+    // print number of arguments
+    printf("Number of arguments: %d\n", argc);
+
+    // print arguments
+    for (int i = 0; i < argc; i++) {
+        printf("Argument %d: %s\n", i, argv[i]);
+    }
+
+    Chunk chunk;
+    initChunk(&chunk);
+    writeChunk(&chunk, OP_RETURN);
+    freeChunk(&chunk);
+
+    return 0;
+}
