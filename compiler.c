976fc52fa232ef0bb84fe390a4fc668f7899fcf4
Mitchell Paulus <paulusm14@gmail.com>
2024-01-24 21:02:47 -0600

    Pg. 309

--- compiler.c
+++ compiler.c
@@ -4,20 +4,27 @@
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char* source) {
-    initScanner(source);
+typedef struct {
+    Token current;
+    Token previous;
+} Parser;
 
-    int line = -1;
+Parser parser;
+
+static void advance() {
+    parser.previous = parser.current;
+
     for (;;) {
-        Token token = scanToken();
-        if (token.line != line) {
-            printf("%4d ", token.line);
-            line = token.line;
-        } else {
-            printf("   | ");
-        }
-        printf("%2d '%.*s'\n", token.type, token.length, token.start);
+        parser.current = scanToken();
+        if (parser.current.type != TOKEN_ERROR) break;
 
-        if (token.type == TOKEN_EOF) break;
+        errorAtCurrent(parser.current.start);
     }
+}
+
+bool compile(const char* source, Chunk* chunk) {
+    initScanner(source);
+    advance();
+    expression();
+    consume(TOKEN_EOF, "Expect end of expression.");
 }
