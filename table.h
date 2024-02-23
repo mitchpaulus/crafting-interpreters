52c0bda09c6c24c279e24879c9dd2e7cd7d427ba
Mitchell Paulus <paulusm14@gmail.com>
2024-02-22 19:52:18 -0600

    Chap 20, Pg. 380

--- table.h
+++ table.h
@@ -20,6 +20,7 @@ void freeTable(Table* table);
 bool tableGet(Table* table, ObjString* key, Value* value);
 bool tableSet(Table* table, ObjString* key, Value value);
 void tableAddAll(Table* from, Table* to);
+ObjString* tableFindString(Table* table, const char* chars, int length, uint32_t hash);
 bool tableDelete(Table* table, ObjString* key);
 
 #endif
